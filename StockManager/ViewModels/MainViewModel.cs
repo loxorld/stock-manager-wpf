e1689c144cba144e22cfbaae55fd27abb986d65e
Brian <brianlautaro@hotmail.com>
2026-01-13 20:34:46 -0300

    Agregar archivos de proyecto.

---
+++ StockManager/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace StockManager.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string titulo = "Stock Manager";
+
+    [ObservableProperty]
+    private int contador = 0;
+
+    [RelayCommand]
+    private void Incrementar() => Contador++;
+}
+
+
